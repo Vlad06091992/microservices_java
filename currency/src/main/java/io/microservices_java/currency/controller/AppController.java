16a88b4f724ae5306db0b73524f3701612aaf662
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-09-22 10:32:32 +0500

    добавил префикс для запросов за котировками валют

--- currency/src/main/java/io/microservices_java/currency/controller/AppController.java
+++ currency/src/main/java/io/microservices_java/currency/controller/AppController.java
@@ -4,6 +4,7 @@ import io.microservices_java.currency.service.CbrService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
@@ -11,6 +12,7 @@ import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 @RestController
+@RequestMapping("currency")
 public class AppController {
 
     @Autowired
