c89f88bf662f6c5d6ec437894e59b817f7d3be5b
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-10-07 11:12:52 +0500

    создал eureka-service для рееста сервисов

---
+++ eureka/src/test/java/io/microservices_java/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.microservices_java.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
